81d74095bc57789d0d8a2b9d6251e9e5935faffa
Yogesh B <ybodke123@gmail.com>
2024-08-26 14:37:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cognifyz_Internship_Task- 
+  1.Develop a front-end interface that interacts with server API.
+  2. Fetch and display data from your API on the front end.
---
+++ script .js
@@ -0,0 +1,73 @@
+const API_KEY = "71036d9beb764af5a62a1aff729651dc";
+const url = "https://newsapi.org/v2/everything?q=";
+
+
+
+async function fetchNews(query){ 
+    const res =  await fetch(`${url}${query}&apiKey=${API_KEY}`)
+    //console.log( await res.json());
+    const data = await res.json();
+    bindData(data.articles);
+    };
+window.addEventListener("load", () => fetchNews("India"));
+
+function reload() {
+    window.location.reload();
+}
+
+
+
+function bindData(articles) {
+    const cardsContainer = document.getElementById("cards-container");
+    const newsCardTemplate = document.getElementById("template-news-card");
+
+    cardsContainer.innerHTML = "";
+
+    articles.forEach((article) => {
+        if (!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fill_Data_In_Card(cardClone, article);
+        cardsContainer.appendChild(cardClone);
+    });
+}
+
+function fill_Data_In_Card(cardClone, article) {
+    const newsImg = cardClone.querySelector("#news-img");
+    const newsTitle = cardClone.querySelector("#news-title");
+    const newsSource = cardClone.querySelector("#news-source");
+    const newsDesc = cardClone.querySelector("#news-desc");
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta",
+    });
+
+    newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+    cardClone.firstElementChild.addEventListener("click", () => {
+        window.open(article.url, "_blank");
+    });
+}
+
+let curSelectedNav = null;
+function onNavItemClick(id) {
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = navItem;
+    curSelectedNav.classList.add("active");
+}
+
+const searchButton = document.getElementById("search-button");
+const searchText = document.getElementById("search-text");
+
+searchButton.addEventListener("click", () => {
+    const query = searchText.value;
+    if (!query) return;
+    fetchNews(query);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = null;
+});
